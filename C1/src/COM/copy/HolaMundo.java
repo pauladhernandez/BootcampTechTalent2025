1b78e6e482759419e3cb83870cb4b1702763a3cd
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-14 13:10:08 +0100

    Actualización

--- C1/src/COM/copy/HolaMundo.java
+++ C1/src/COM/copy/HolaMundo.java
@@ -1,10 +1,10 @@
-package COM;
+package COM.copy;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo") ;
+		System.out.println("Hola jueves") ;
 	}
 
 }
