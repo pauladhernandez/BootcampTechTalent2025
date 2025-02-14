39cdca2b477ea624162ea3c7c36ab2c18856f259
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-14 13:54:47 +0100

    Update Tarea4.java

--- C2-JavaBasics/src/Tarea4.java
+++ C2-JavaBasics/src/Tarea4.java
@@ -3,7 +3,7 @@ public class Tarea4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Tarea 4");
+		System.out.println("Tarea 4.1");
 	}
 
 }
