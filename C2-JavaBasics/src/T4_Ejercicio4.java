e8c54c3238d0ba8326b23cc0ccfb748ac657fc0a
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-16 20:36:37 +0100

    Create T4_Ejercicio4.java

---
+++ C2-JavaBasics/src/T4_Ejercicio4.java
@@ -0,0 +1,18 @@
+
+public class T4_Ejercicio4 {
+
+	public static void main(String[] args) {
+	
+//		Declarar la variable con un valor
+	int N=90;
+	
+//	Resultaldo de incrementar, decrementar y duplicar
+	System.out.println("Valor inicial de N = " + N);
+	System.out.println("N + 77: " + (N+77));
+	System.out.println("N - 3: " + (N-3));
+	System.out.println("N * 2: " + (N*2));
+	
+
+	}
+
+}
