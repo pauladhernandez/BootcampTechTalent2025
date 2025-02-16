a5a1625ba8a79952e14b13ae4fe27b79cc28fa72
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-16 19:58:21 +0100

    Update T4_Ejercicio3.java

--- C2-JavaBasics/src/T4_Ejercicio3.java
+++ C2-JavaBasics/src/T4_Ejercicio3.java
@@ -7,21 +7,44 @@ public class T4_Ejercicio3 {
 		double n=10.5; double m=30.8;
 		
 //		Valores de las variables
-		System.out.println("Variable x = " + x);
-		System.out.println("Variable de y = " y);
+		System.out.println("Variable de x = " + x);
+		System.out.println("Variable de y = " + y);
 		System.out.println("Variable de n = " + n);
 		System.out.println("Variable de m = " + m);
 		
 //		Operaciones X y Y
 		System.out.println("Suma de X + Y = " + (x+y) );
 		System.out.println("Resta de X - Y = " + (x-y));
-		System.out.println("Multiplicacion de X * Y =" + (x*y);
+		System.out.println("Multiplicacion de X * Y =" + (x*y));
 		System.out.println("Division de X / Y = " + (x/y));
 		System.out.println("Resto X % Y = " + (x%y));
 		
 //		Operaciones N y M
 		System.out.println("Suma de N + M = " + (n+m));
 		System.out.println("Resta de N - M = " + (n-m));
+		System.out.println("Multiplicacion de N * M = " + (n*m));
+		System.out.println("Division de N / M = " + (n/m));
+		System.out.println("Resto de N % M = " + (n%m));
+		
+//		Operaciones cruzadas de X Y N M
+		System.out.println("Suma de X + N = " + (x+n));
+		System.out.println("Division de Y / M = " + (y/m));
+		System.out.println("Resto de Y % M = " + (y%m));
+		
+//		Doble de cada variable
+		System.out.println("Doble de X = " + (x*2));
+		System.out.println("Doble de Y = " + (y*2));
+		System.out.println("Doble de N = " + (n*2));
+		System.out.println("Doble de M = " + (m*2));
+		
+//		Suma de toda las variables
+		System.out.println("Suma de todas las variables: " + (x+y+n+m));
+		
+//		Multiplicacion de todas las variables
+		System.out.println("Multiplicacion de todas las variables: " + (x*y*n*m));
+		
+		
+		
 		
 		
 		
