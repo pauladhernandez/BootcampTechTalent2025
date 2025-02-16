c42453c5568eea39fdc378c8edc9d1d51b618af7
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-16 19:23:18 +0100

    Create T4_Ejercicio3.java

---
+++ C2-JavaBasics/src/T4_Ejercicio3.java
@@ -0,0 +1,32 @@
+
+public class T4_Ejercicio3 {
+
+	public static void main(String[] args) {
+//		Asignación de variables
+		int x=20; int y=80;
+		double n=10.5; double m=30.8;
+		
+//		Valores de las variables
+		System.out.println("Variable x = " + x);
+		System.out.println("Variable de y = " y);
+		System.out.println("Variable de n = " + n);
+		System.out.println("Variable de m = " + m);
+		
+//		Operaciones X y Y
+		System.out.println("Suma de X + Y = " + (x+y) );
+		System.out.println("Resta de X - Y = " + (x-y));
+		System.out.println("Multiplicacion de X * Y =" + (x*y);
+		System.out.println("Division de X / Y = " + (x/y));
+		System.out.println("Resto X % Y = " + (x%y));
+		
+//		Operaciones N y M
+		System.out.println("Suma de N + M = " + (n+m));
+		System.out.println("Resta de N - M = " + (n-m));
+		
+		
+		
+			
+
+	}
+
+}
