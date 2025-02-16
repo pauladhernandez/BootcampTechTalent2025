972b6e6aaedf36cd7be81212612d580609ba51b5
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-16 18:40:08 +0100

    Ejercicios 1 y 2 tarea 4 en casa

---
+++ C2-JavaBasics/src/T4_Ejercicio1.java
@@ -0,0 +1,27 @@
+
+public class T4_Ejercicio1 {
+
+	public static void main(String[] args) {
+//		Declarar dos variables numéricas
+		int num1=50;
+		int num2=20;
+				
+//			Operaciones matemáticas
+				
+			int suma = num1 + num2;
+			int resta = num1 - num2;
+			int multiplicacion = num1 * num2;
+			double division = (double) num1 / num2;
+			int modulo = num1 % num2;
+			
+//		Resultados
+				System.out.println("Suma: " + suma);
+				System.out.println("Resta: " + resta);
+				System.out.println("Multiplicacion: " + multiplicacion);
+				System.out.println("Division: " + division);
+				System.out.println("Modulo: " + modulo);
+				
+			
+	}
+
+}
---
+++ C2-JavaBasics/src/T4_Ejercicio2.java
@@ -0,0 +1,24 @@
+
+public class T4_Ejercicio2 {
+
+	public static void main(String[] args) {
+//		Declaramos las variables a cada tipo (int, double, char)
+		int n=20;
+		double a=10.5;
+		char letra='p';
+		
+//		Operaciones matemáticas de las variables
+		double suma = n + a;
+		double resta = a - n;
+		int valorNumerico = (int) letra;
+		
+		System.out.println("Variable N = 20");
+		System.out.println("Variable A = 10.5");
+		System.out.println("Variable C = p");
+		System.out.println("20 + 10.5 = " + suma);
+		System.out.println("10.5 - 20 = " + resta);
+		System.out.println("Valor numérico del carácter p: " + valorNumerico );
+			
+	}
+
+}
