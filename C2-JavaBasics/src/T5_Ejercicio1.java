8af8634c70deb5c1ffe70bccb437bc7f3e267d6c
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-17 12:40:42 +0100

    Create T5_Ejercicio1.java

---
+++ C2-JavaBasics/src/T5_Ejercicio1.java
@@ -0,0 +1,18 @@
+
+public class T5_Ejercicio1 {
+
+	public static void main(String[] args) {
+	
+		int num1=25; int num2=30;
+		
+		if (num1 > num2) {
+		System.out.println(num1 + "El valor es mayor que " + (num2));
+		} else if (num2 > num1) {
+		System.out.println(num2 + " es mayor que " + num1);
+
+		} else {
+		System.out.println(num1 + " y " + num2 + "son iguales");
+		}
+	}
+
+}
