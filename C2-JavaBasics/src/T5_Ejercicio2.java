fe12e821e6da3076d8ed77ce0beedbb91c6be774
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-17 13:11:43 +0100

    Avance tarea 5

---
+++ C2-JavaBasics/src/T5_Ejercicio2.java
@@ -0,0 +1,11 @@
+
+public class T5_Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Paula";
+		System.out.println("Bienvenida, " + nombre);
+		
+	}
+
+}
---
+++ C2-JavaBasics/src/T5_Ejercicio3.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class T5_Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		String nombre=JOptionPane.showInputDialog("Paula Díaz");
+		
+		JOptionPane.showMessageDialog(null," Mi nombre es " + nombre);
+
+	}
+
+}
