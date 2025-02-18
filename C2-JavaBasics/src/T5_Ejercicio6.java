203d3731c0e09876de6c891d33bae695b2b40132
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-18 13:11:49 +0100

    Create T5_Ejercicio6.java

---
+++ C2-JavaBasics/src/T5_Ejercicio6.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class T5_Ejercicio6 {
+
+	public static void main(String[] args) {
+		
+		final double IVA=0.21;
+		String text_num=JOptionPane.showInputDialog("Introduce el precio del producto");
+		
+		int precio=Integer.parseInt(text_num);
+		JOptionPane.showMessageDialog(null, "El producto tiene un precio de "+precio+""
+				+ " y su precio final es de "+(precio+(precio*IVA)));
+		
+	}
+
+}
