db1b2381090bbd89df82e37a25c3d369aaf3b40b
pauladhernandez <mpaula.dhdz@gmail.com>
2025-02-21 12:30:39 +0100

    Ejercicio 8 tarea 5

---
+++ C2-JavaBasics/src/UD_05_Tareas/T5_Ejercicio8.java
@@ -0,0 +1,14 @@
+package UD_05_Tareas;
+
+public class T5_Ejercicio8 {
+
+	public static void main(String[] args) {
+		
+		
+			for (int numero=1; numero<=100; numero++) {
+			System.out.println(numero);
+			
+		}	
+	}
+
+}
