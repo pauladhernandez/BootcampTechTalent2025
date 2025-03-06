7b5dfda222867f881d12fe8aa745ff2fea3dfe50
pauladhernandez <mpaula.dhdz@gmail.com>
2025-03-06 13:58:24 +0100

    Update ejercicio11.java

--- C2-JavaBasics/src/UD06MetodosyArrays/UD_06_Teoria/ejercicio11.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD_06_Teoria/ejercicio11.java
@@ -3,60 +3,65 @@ package UD06MetodosyArrays.UD_06_Teoria;
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class ejercicio11 {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Introduce la dimensión de los arrays: ");
-		Random scanner; //preguntar
-		int dimension = scanner.nextInt();
-		
-		 // preguntar
-		
-		int[] array1 = new int[dimension];
-		// array1 se rellena de randoms con el método
-		array1 = rellenarArray1(dimension);
-		
-		
-		
-		int[] array2 = new int[dimension];
-		int multiplicado[] = new int[dimension];
-		
-		
-//		for (int i = 0; i < dimension; i++) { //preguntar
-//			int[] array;
-//			Random rand;
-//			array[i] = rand.nextInt(10) + 1;
-//		}
-	
-		array2 = array1;
-		
-		for (int i = 0; i < dimension; i++) { // preguntar
-			Random rand;
-			array2[i] = rand.nextInt(10) + 1;
-		}
 
-	
-	}
-	
-	public static int[] rellenarArray1(int tamany) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce la dimensión de los arrays: ");
+		Random scanner;
+		int dimension = sc.nextInt();
+
 		
+		int[] array1 = rellenarArray1[dimension]; // array1 se rellena de randoms con el método
+
+//		array1 = rellenarArray1(dimension);
+
+		int[] array2 = new int[dimension];
+		
+		
+		
+		int multiplicado[] = new int[dimension];
+
+//		Imprimir los arrays
+		System.out.println();
+	}
+
+	}
+
+	public static int[] x(int tamany) {
+
 		Random aleatorio = new Random();
 		int[] arrayPrimero;
-		
-		for (int i = 0; i < tamany; i++) { //preguntar
-			
-			arrayPrimero[i] = aleatorio.nextInt(0,11);
+
+		for (int i = 0; i < tamany; i++) {
+
+			arrayPrimero[i] = aleatorio.nextInt(0, 11);
 		}
-		//arrayPrimero = {8,5,3}
-		
+		// arrayPrimero = {8,5,3}
+
 		return arrayPrimero;
 	}
 
-	public static void rellenarNumeros (int array1, int [] array2) {
+	public static int[] rellenarNumeros(int[] array1, int[] array2) {
+
+		Random aleatorio = new Random();
+		int[] arraySegundo;
+		for (int i = 0; i < array1.length; i++) {
+			array2[i] = aleatorio.nextInt(0, 11);
+
+		}
+
+		return arraySegundo;
 	}
-	
-}
-}
+
+	public static int[] multiplicarArrays(int[] array1, int[] array2) {
+
+		int[] resultado = new int[array1.length];
+		for (int i = 0; i < array1.length; i++) {
+			resultado[i] = array1[i] * array2[i];
+		}
+		return resultado;
+	}
+
+}}
