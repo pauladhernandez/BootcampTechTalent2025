bd33a1c17e825cdebe118291ac987b4a15436558
pauladhernandez <mpaula.dhdz@gmail.com>
2025-03-21 14:01:55 +0100

    ejercicios t7

--- C2-JavaBasics/src/UD07ArraylistyHashtable/UD_07_Tareas/T7_Ejercicio03.java
+++ C2-JavaBasics/src/UD07ArraylistyHashtable/UD_07_Tareas/T7_Ejercicio03.java
@@ -1,102 +1,95 @@
 package UD07ArraylistyHashtable.UD_07_Tareas;
 
+
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class T7_Ejercicio03 {
+	
+	    private static ArrayList<String> listaProductos = new ArrayList<>();
+	    private static HashMap<String, Integer> stockProductos = new HashMap<>();
+	    private static HashMap<String, Double> preciosProductos = new HashMap<>();
 
-	private static HashMap<String, Double> stockProductos = new HashMap<>();
+	    public static void main(String[] args) {
+	        inicializarProductos();
+	        listarProductos();
+	        // Simulación de la interfaz visual para añadir productos
+	        agregarProductoDesdeConsola();
+	        consultarProducto();
+	    }
 
-	public static void main(String[] args) {
+	    private static void inicializarProductos() {
+	        listaProductos.add("Gafas");
+	        listaProductos.add("Pulsera");
+	        listaProductos.add("Ordenador");
+	        listaProductos.add("Funda");
+	        listaProductos.add("Ratón");
+	        listaProductos.add("Soporte");
+	        listaProductos.add("Cargador");
+	        listaProductos.add("Escritorio");
+	        listaProductos.add("Reloj");
+	        listaProductos.add("Audífonos");
 
-		// Diccionario de artículos con sus precios
-		Map<String, Double> stockProductos = new HashMap<>();
+	        preciosProductos.put("Gafas", 10.00);
+	        preciosProductos.put("Pulsera", 5.00);
+	        preciosProductos.put("Ordenador", 100.00);
+	        preciosProductos.put("Funda", 13.00);
+	        preciosProductos.put("Ratón", 10.00);
+	        preciosProductos.put("Soporte", 2.00);
+	        preciosProductos.put("Cargador", 90.00);
+	        preciosProductos.put("Escritorio", 50.00);
+	        preciosProductos.put("Reloj", 90.00);
+	        preciosProductos.put("Audífonos", 100.00);
 
-		// Llenamos el diccionario con 10 artículos iniciales
-		stockProductos.put("Leche", 1.20);
-		stockProductos.put("Pan", 0.90);
-		stockProductos.put("Arroz", 2.50);
-		stockProductos.put("Azúcar", 1.80);
-		stockProductos.put("Cereal", 3.00);
-		stockProductos.put("Jabón", 2.20);
-		stockProductos.put("Aceite", 4.50);
-		stockProductos.put("Tomates", 2.00);
-		stockProductos.put("Manzanas", 1.50);
-		stockProductos.put("Café", 5.00);
+	        stockProductos.put("Gafas", 10);
+	        stockProductos.put("Pulsera", 5);
+	        stockProductos.put("Ordenador", 100);
+	        stockProductos.put("Funda", 13);
+	        stockProductos.put("Ratón", 10);
+	        stockProductos.put("Soporte", 2);
+	        stockProductos.put("Cargador", 90);
+	        stockProductos.put("Escritorio", 50);
+	        stockProductos.put("Reloj", 90);
+	        stockProductos.put("Audífonos", 100);
+	    }
 
-		Scanner scanner = new Scanner(System.in);
+	    private static void agregarProductoDesdeConsola() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Ingrese el nombre del producto: ");
+	        String nombre = scanner.nextLine();
+	        System.out.print("Ingrese el precio del producto: ");
+	        double precio = scanner.nextDouble();
+	        System.out.print("Ingrese la cantidad del producto: ");
+	        int cantidad = scanner.nextInt();
 
-		while (true) {
-			System.out.println("\n--- Gestión de Stock de Productos ---");
-			System.out.println("1. Añadir artículo");
-			System.out.println("2. Consultar artículo");
-			System.out.println("3. Listar todos los artículos");
-			System.out.println("4. Salir");
-			System.out.print("Seleccione una opción: ");
+	        listaProductos.add(nombre);
+	        preciosProductos.put(nombre, precio);
+	        stockProductos.put(nombre, cantidad);
+	        System.out.println("Producto agregado exitosamente.");
+	    }
+	    	
 
-			int opcion = scanner.nextInt();
-			scanner.nextLine(); // Limpiar el buffer
+	    private static void consultarProducto() {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Ingrese el nombre del producto a consultar: ");
+	        String nombre = scanner.nextLine();
 
-			switch (opcion) {
-			case 1:
-				añadirArticulo(scanner);
-				break;
-			case 2:
-				consultarArticulo(scanner);
-				break;
-			case 3:
-				listarArticulos();
-				break;
-			case 4:
-				System.out.println("Saliendo del programa...");
-				scanner.close();
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Opción no válida. Inténtelo nuevamente.");
-			}
-		}
+	        if (preciosProductos.containsKey(nombre)) {
+	            System.out.println("Producto: " + nombre);
+	            System.out.println("Precio: €" + preciosProductos.get(nombre));
+	            System.out.println("Cantidad en stock: " + stockProductos.get(nombre));
+	        } else {
+	            System.out.println("El producto no existe en el inventario.");
+	        }
+	    }
+
+	    private static void listarProductos() {
+	        System.out.println("\nInventario:");
+	        for (String producto : listaProductos) {
+	            System.out.println(producto + " - €" + preciosProductos.get(producto) + "\nCantidad en stock: " + stockProductos.get(producto));
+	        }
+	        
+	    }
 	}
 
-	// Método para añadir un artículo al diccionario
-	private static void añadirArticulo(Scanner scanner) {
-		System.out.print("Ingrese el nombre del artículo: ");
-		String articulo = scanner.nextLine();
-
-		System.out.print("Ingrese el precio del artículo: ");
-		double precio = scanner.nextDouble();
-		scanner.nextLine(); // Limpiar el buffer
-
-		// Añadir el artículo al diccionario
-		stockProductos.put(articulo, (double) precio);
-		System.out.println("Artículo '" + articulo + "' añadido con éxito.");
-	}
-
-	// Método para consultar el precio de un artículo
-	private static void consultarArticulo(Scanner scanner) {
-		System.out.print("Ingrese el nombre del artículo a consultar: ");
-		String articulo = scanner.nextLine();
-
-		// Verificar si el artículo está en el diccionario
-		if (stockProductos.containsKey(articulo)) {
-			double precio = stockProductos.get(articulo);
-			System.out.println("El precio de " + articulo + " es: " + precio + " €.");
-		} else {
-			System.out.println("El artículo '" + articulo + "' no se encuentra en el stock.");
-		}
-	}
-
-	// Método para listar todos los artículos en el stock
-	private static void listarArticulos() {
-		if (stockProductos.isEmpty()) {
-			System.out.println("No hay artículos en el stock.");
-		} else {
-			System.out.println("\nListado de artículos en el stock:");
-			for (Entry<String, Double> entry : stockProductos.entrySet()) {
-				System.out.println("Artículo: " + entry.getKey() + " - Precio: " + entry.getValue() + " €");
-			}
-		}
-	}
-}
--- C2-JavaBasics/src/UD07ArraylistyHashtable/UD_07_Tareas/t7.java
+++ C2-JavaBasics/src/UD07ArraylistyHashtable/UD_07_Tareas/t7.java
@@ -1,87 +1,48 @@
 package UD07ArraylistyHashtable.UD_07_Tareas;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class t7{
+		
+	private static 	ArrayList<String> listaProductos=new ArrayList<String>();
+//		ArrayList<Integer> listaStock = new ArrayList<>();
+//		ArrayList<String> listaBootcamp = new Arraylist<>();
+		
+		public static void main(String [] args) {
+			inicializarProductos();
+			listarProductos();
+		}
+		private static void inicializarProductos() {
+		
+		listaProductos.add("Gafas, 10€");
+		listaProductos.add("Pulsera, 5€");
+		listaProductos.add("Ordenador, 100€");
+		listaProductos.add("Funda, 13€");
+		listaProductos.add("Ratón, 10€");
+		listaProductos.add("Soporte, 2€");
+		listaProductos.add("Cargador, 90€");
+		listaProductos.add("Escritorio, 50€");
+		listaProductos.add("Reloj, 90€");
+		listaProductos.add("Audífonos, 100€");
+		
+		}
+		
+		private static void listarProductos() {
+			System.out.println("\nInventario: ");
+			for (Object primera : listaProductos) {
+				System.out.println(primera);
+			
+		}
+	
+		}
+		
+//		System.out.println(listaProductos.toString());
+//		System.out.println(listaProductos.size());
+		
+//		listaBootcamp.add()
+		
+		
+	}
+	
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Double> carrito = new ArrayList<>();
 
-        int numArticulos = obtenerNumeroDeArticulos(scanner);
-        añadirArticulosAlCarrito(scanner, carrito, numArticulos);
-
-        double precioTotalBruto = calcularPrecioTotalBruto(carrito);
-        double precioConIva = calcularPrecioConIva(scanner, precioTotalBruto);
-
-        mostrarResultados(numArticulos, precioTotalBruto, precioConIva);
-
-        double cantidadPagada = obtenerCantidadPagada(scanner);
-        calcularYMostrarCambio(precioConIva, cantidadPagada);
-
-        scanner.close();
-    }
-
-    private static int obtenerNumeroDeArticulos(Scanner scanner) {
-        System.out.print("Ingrese el número de artículos en el carrito: ");
-        return scanner.nextInt();
-    }
-
-    private static void añadirArticulosAlCarrito(Scanner scanner, ArrayList<Double> carrito, int numArticulos) {
-        for (int i = 0; i < numArticulos; i++) {
-            System.out.print("Ingrese el precio del artículo " + (i + 1) + ": ");
-            double precio = scanner.nextDouble();
-            carrito.add(precio);
-        }
-    }
-
-    private static double calcularPrecioTotalBruto(ArrayList<Double> carrito) {
-        double precioTotalBruto = 0.0;
-        for (double precio : carrito) {
-            precioTotalBruto += precio;
-        }
-        return precioTotalBruto;
-    }
-
-    private static double calcularPrecioConIva(Scanner scanner, double precioTotalBruto) {
-        System.out.println("Introduce tipo iva(21% o 4%): ");
-        double Iva = scanner.nextDouble();
-        double iva1 = 0.21;
-        double iva2 = 0.04;
-        double precioConIva;
-
-        if (Iva == 21) {
-            precioConIva = precioTotalBruto * (1 + iva1);
-            System.out.println("IVA aplicado: " + (iva1 * 100) + "%");
-        } else if (Iva == 4) {
-            precioConIva = precioTotalBruto * (1 + iva2);
-            System.out.println("IVA aplicado: " + (iva2 * 100) + "%");
-        } else {
-            System.out.println("Tipo de IVA no válido. Se aplicará el IVA del 21% por defecto.");
-            precioConIva = precioTotalBruto * (1 + iva1);
-            System.out.println("IVA aplicado: " + (iva1 * 100) + "%");
-        }
-        return precioConIva;
-    }
-
-    private static void mostrarResultados(int numArticulos, double precioTotalBruto, double precioConIva) {
-        System.out.println("Número de artículos comprados: " + numArticulos);
-        System.out.println("Precio total bruto: " + precioTotalBruto + " €");
-        System.out.println("Precio total con IVA: " + precioConIva + " €");
-    }
-
-    private static double obtenerCantidadPagada(Scanner scanner) {
-        System.out.print("Ingrese la cantidad pagada por el cliente: ");
-        return scanner.nextDouble();
-    }
-
-    private static void calcularYMostrarCambio(double precioConIva, double cantidadPagada) {
-        double cambio = cantidadPagada - precioConIva;
-        if (cambio < 0) {
-            System.out.println("El cliente no ha pagado suficiente. Faltan " + (-cambio) + " €.");
-        } else {
-            System.out.println("Cambio a devolver: " + cambio + " €");
-        }
-    }
-}
