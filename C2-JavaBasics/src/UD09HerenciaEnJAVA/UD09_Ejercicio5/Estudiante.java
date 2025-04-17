9eadb148402a4d39bcd00b6d1cb66afaf818627d
pauladhernandez <mpaula.dhdz@gmail.com>
2025-04-17 09:58:24 +0200

    Create Estudiante.java

---
+++ C2-JavaBasics/src/UD09HerenciaEnJAVA/UD09_Ejercicio5/Estudiante.java
@@ -0,0 +1,22 @@
+package UD09HerenciaEnJAVA.UD09_Ejercicio5;
+
+public class Estudiante extends Persona {
+ private double calificacion;
+
+ public Estudiante(String nombre, int edad, char sexo, double calificacion) {
+     super(nombre, edad, sexo);
+     this.calificacion = calificacion;
+ }
+
+ public boolean estaDisponible() {
+     return Math.random() > 0.5; // 50% de probabilidad de asistir
+ }
+
+ public boolean estaAprobado() {
+     return calificacion >= 5;
+ }
+
+ public char getSexo() {
+     return sexo;
+ }
+} 
