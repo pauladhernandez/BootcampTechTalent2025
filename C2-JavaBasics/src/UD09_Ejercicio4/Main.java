e1b65fed9f4e3c7a7fef367549dd50d0fc2642c2
pauladhernandez <mpaula.dhdz@gmail.com>
2025-04-17 09:34:40 +0200

    ejercicio 4

---
+++ C2-JavaBasics/src/UD09_Ejercicio4/Main.java
@@ -0,0 +1,19 @@
+package UD09_Ejercicio4;
+
+public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto Raices con coeficientes
+        Raices ecuacion1 = new Raices(1, -3, 2); // Tiene dos soluciones: x=1 y x=2
+        Raices ecuacion2 = new Raices(1, 2, 1);  // Tiene una sola solución: x=-1
+        Raices ecuacion3 = new Raices(1, 1, 5);  // No tiene soluciones reales
+
+        System.out.println("Ecuación 1:");
+        ecuacion1.calcular();
+
+        System.out.println("\nEcuación 2:");
+        ecuacion2.calcular();
+
+        System.out.println("\nEcuación 3:");
+        ecuacion3.calcular();
+    }
+}
---
+++ C2-JavaBasics/src/UD09_Ejercicio4/Raices.java
@@ -0,0 +1,59 @@
+package UD09_Ejercicio4;
+
+public class Raices {
+    private double a;
+    private double b;
+    private double c;
+
+    // Constructor
+    public Raices(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // Método para obtener el discriminante
+    public double getDiscriminante() {
+        return (b * b) - (4 * a * c);
+    }
+
+    // Método para saber si tiene dos raíces
+    public boolean tieneRaices() {
+        return getDiscriminante() > 0;
+    }
+
+    // Método para saber si tiene una raíz
+    public boolean tieneRaiz() {
+        return getDiscriminante() == 0;
+    }
+
+    // Método para obtener las dos raíces
+    public void obtenerRaices() {
+        if (tieneRaices()) {
+            double discriminante = getDiscriminante();
+            double raiz1 = (-b + Math.sqrt(discriminante)) / (2 * a);
+            double raiz2 = (-b - Math.sqrt(discriminante)) / (2 * a);
+            System.out.println("Raíz 1: " + raiz1);
+            System.out.println("Raíz 2: " + raiz2);
+        }
+    }
+
+    // Método para obtener una única raíz
+    public void obtenerRaiz() {
+        if (tieneRaiz()) {
+            double raiz = -b / (2 * a);
+            System.out.println("Única raíz: " + raiz);
+        }
+    }
+
+    // Método general que calcula y muestra las soluciones
+    public void calcular() {
+        if (tieneRaices()) {
+            obtenerRaices();
+        } else if (tieneRaiz()) {
+            obtenerRaiz();
+        } else {
+            System.out.println("No existen soluciones reales.");
+        }
+    }
+}
