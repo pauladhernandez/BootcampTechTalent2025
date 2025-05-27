c57479ce068f3a340e2b1560935694aad13e9e14
pauladhernandez <mpaula.dhdz@gmail.com>
2025-05-27 10:43:20 +0200

    COSITAS

--- CALCULADORA/src/CalculadoraApp.java
+++
@@ -1,7 +0,0 @@
-
-public class CalculadoraApp {
-    public static void main(String[] args) {
-        // Inicia la interfaz gráfica en el hilo de eventos de Swing (buena práctica)
-        javax.swing.SwingUtilities.invokeLater(VentanaCalculadora::new);
-    }
-}
